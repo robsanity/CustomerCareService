077afc173413e219f50eb373a0bdf67690b67ac0
ROBERTO DI CIAULA S299444 <s299444@studenti.polito.it>
2023-04-14 15:53:00 +0200

    validation final version

--- server/src/main/kotlin/it/polito/wa2/g35/server/profiles/ProfileDTO.kt
+++ server/src/main/kotlin/it/polito/wa2/g35/server/profiles/ProfileDTO.kt
@@ -1,15 +1,16 @@
 package it.polito.wa2.g35.server.profiles
 
+import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
-import org.jetbrains.annotations.NotNull
+
 
 data class ProfileDTO(
-    @field:Size(min=2,max=40) @NotBlank
+    @field:Email(regexp = ".*"+"@"+".*"+"\\."+".*") @Size(min=4,max=40) @NotBlank
     val email: String,
-    @field:Size(min=2,max=20)
+    @field:Size(min=2,max=20) @NotBlank
     var name: String,
-    @field:Size(min=2,max=20)
+    @field:Size(min=2,max=20) @NotBlank
     var surname: String
 )
 

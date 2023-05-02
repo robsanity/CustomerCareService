e902364b41a05fcdf6c6030bf32184592881f921
Michele Galati <s305937@studenti.polito.it>
2023-05-02 20:36:58 +0200

    fix attachmentsServiceImpl

--- server/src/main/kotlin/it/polito/wa2/g35/server/ticketing/Attachment/AttachmentServiceImpl.kt
+++ server/src/main/kotlin/it/polito/wa2/g35/server/ticketing/Attachment/AttachmentServiceImpl.kt
@@ -8,11 +8,6 @@ import org.springframework.stereotype.Service
 
 @Service
 class AttachmentServiceImpl(private val attachmentRepository: AttachmentRepository) : AttachmentService {
-    override fun getAttachment(attachmentId: String): AttachmentDTO? {
-        TODO("Not yet implemented")
-    }
-
-<<<<<<< HEAD
     override fun getAttachments(attachmentId: Long?): List<AttachmentDTO> {
        return attachmentRepository.getAttachments(attachmentId)?.map { it.toDTO() } ?: emptyList()
     }
@@ -24,9 +19,4 @@ class AttachmentServiceImpl(private val attachmentRepository: AttachmentReposito
         else{
             throw BadRequestException("Bad request format!")
         }
-=======
-    override fun postAttachment(attachment: AttachmentDTO?): AttachmentDTO? {
-        TODO("Not yet implemented")
->>>>>>> 6bf6d54fc732a10f3f76acb38687d01b8f0758bb
-    }
 }

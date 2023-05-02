1f204a381b33ab63df8a44557d4b0a0079ca5eee
ROBERTO DI CIAULA S299444 <s299444@studenti.polito.it>
2023-05-02 19:44:33 +0200

    tries

--- server/src/main/kotlin/it/polito/wa2/g35/server/ticketing/Ticket/TicketRepository.kt
+++ server/src/main/kotlin/it/polito/wa2/g35/server/ticketing/Ticket/TicketRepository.kt
@@ -1,17 +1,22 @@
 package it.polito.wa2.g35.server.ticketing.Ticket
 
+import it.polito.wa2.g35.server.profiles.Employee.Expert.Expert
 import it.polito.wa2.g35.server.ticketing.Order.Order
+import it.polito.wa2.g35.server.ticketing.TicketStatus.TicketStatusValues
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface TicketRepository: JpaRepository<Ticket, Long> {
-    @Query("SELECT t from Ticket t WHERE t.id =: idTicket")
-    fun getTicketById(idTicket: String): Ticket?
 
-    //@Query("SELECT t FROM Order t WHERE t.customer.email = :idCustomer AND t.product.id = :idProduct")
-    //fun getOrdersByCustomerAndProduct(idCustomer: String, idProduct: String): Order?
-     fun
+     fun getTicketsByStatus(status: TicketStatusValues) : List<Ticket>?
+
+     fun getTicketsByExpert(idExpert: String) : List<Ticket>?
+
+     fun getTicketsByPriority(priority: TicketPriority) : List<Ticket>?
+
+     fun getTicketsByCustomer(idCustomer: String) : List<Ticket>?
+
 }
 
